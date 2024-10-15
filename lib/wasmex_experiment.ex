c251a74d243526295c9ff30557ed301c55e7df77
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-15 21:21:28 +0900

    Rustのテストに変更

--- lib/wasmex_experiment.ex
+++ lib/wasmex_experiment.ex
@@ -5,12 +5,11 @@ defmodule WasmexExperiment do
 
   def hello do
 
-    #{:ok, bytes} = File.read("./hoge_wasm/target/wasm32-unknown-unknown/debug/hoge_wasm.wasm")
-    {:ok, bytes} = File.read("./onyx/test.wasm")
+    {:ok, bytes} = File.read("./hoge_wasm/target/wasm32-unknown-unknown/debug/hoge_wasm.wasm")
+    #{:ok, bytes} = File.read("./onyx/add.wasm")
     {:ok, module} = Wasmex.Module.compile(bytes)
     {:ok, instance} = Wasmex.start_link(%{module: module})
-    #{:ok, instance} = Wasmex.start_link(%{bytes: bytes})
-    #Wasmex.call_function(instance, "hoge", [10])
+    Wasmex.call_function(instance, "hoge", [10])
     |> IO.inspect()
 
     :world

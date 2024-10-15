4cb1a5e080101f42af9924ba607a1156f97629f6
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-15 20:17:56 +0900

    検討　失敗作とりあえずコミット

--- lib/wasmex_experiment.ex
+++ lib/wasmex_experiment.ex
@@ -4,10 +4,13 @@ defmodule WasmexExperiment do
   """
 
   def hello do
-    {:ok, bytes} = File.read("./hoge_wasm/target/wasm32-unknown-unknown/debug/hoge_wasm.wasm")
+
+    #{:ok, bytes} = File.read("./hoge_wasm/target/wasm32-unknown-unknown/debug/hoge_wasm.wasm")
+    {:ok, bytes} = File.read("./onyx/test.wasm")
     {:ok, module} = Wasmex.Module.compile(bytes)
     {:ok, instance} = Wasmex.start_link(%{module: module})
-    Wasmex.call_function(instance, "hoge", [10])
+    #{:ok, instance} = Wasmex.start_link(%{bytes: bytes})
+    #Wasmex.call_function(instance, "hoge", [10])
     |> IO.inspect()
 
     :world
--- onyx/run.sh
+++ onyx/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-onyx build -r wasi -o test.wasm test.onyx
-wasmer run test.wasm
+#onyx build -r wasi -o test.wasm test.onyx
+#onyx build -o test.wasm -r js test.onyx
+#wasmer run test.wasm
+onyx build  -o add.wasm -r js add.onyx
